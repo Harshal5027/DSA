5102e5b4cc5c7409e352a318af2ea59237d76cbb
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-08-08 23:11:14 +0530

    Move Array Folder

---
+++ dsadaily/src/Assignments/Array/Array_Basic/Program13.java
@@ -0,0 +1,20 @@
+package Assignments.Array_Basic;
+
+public class Program13 {
+
+    static void uniqueElement(int arr[], int size){
+
+        
+
+    }
+ 
+    public static void main(String[] args) {
+        
+        int arr [] = new int []{6,2,5,2,2,6,6};
+        int N = arr.length;
+
+        uniqueElement(arr, N);
+
+    }
+
+}
