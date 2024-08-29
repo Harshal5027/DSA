8f86a455d2530e55aacd4e5786e21d3d654fc8b2
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-08-29 20:24:33 +0530

    Check array is sorted in non decreasing order or not

---
+++ dsadaily/Assignments/Array/Array_Easy/Program7.java
@@ -0,0 +1,30 @@
+
+//      Check array is sorted in non decreasing order or not
+
+package Assignments.Array.Array_Easy;
+
+public class Program7 {
+
+    static void sortedOrNot( int arr[], int N){
+
+        for( int i = 0; i < N-1 ; i++){
+
+            if( arr[i] > arr[i+1]){
+
+                System.out.println( "Given array is not Sorted ");
+
+                return ;
+
+            }
+        }
+
+        System.out.println(" Given array is sorted ");
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = { 90, 80, 10, 70, 30, 40};
+
+        sortedOrNot( arr, arr.length);
+    }
+    
+}
