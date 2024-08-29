7b474aa8692e4a3e8aa3f7c0f5310eb426fd3752
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-08-29 20:04:23 +0530

    Count Pair with given Sum

---
+++ dsadaily/Assignments/Array/Array_Easy/Program4.java
@@ -0,0 +1,41 @@
+
+//      Count pair with given sum
+
+package Assignments.Array.Array_Easy;
+
+public class Program4 {
+
+    static int countPairOfSum( int arr[] , int N, int K){
+
+        int count = 0;
+
+        for( int i = 0; i < N; i++){
+
+            for( int j = i+1; j < N; j++){
+
+                if( arr[i] + arr[j] == K ){
+
+                    // System.out.println(arr[i] + arr[j]);
+
+                    count++;
+                }
+            }
+        }
+        if( count > 0){
+
+            return count;
+        }else{
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = { 1, 1, 1, 1};
+
+        int K = 2;
+
+        System.out.println(countPairOfSum( arr, arr.length, K ));
+    }
+    
+}
