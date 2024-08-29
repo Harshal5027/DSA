40b8abdbf4960b41e1df1279fd7e68043b49fe67
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-08-29 23:29:09 +0530

    Transition array

---
+++ dsadaily/Assignments/Array/Array_Easy/Program9.java
@@ -0,0 +1,29 @@
+
+//      Transition Point
+
+package Assignments.Array.Array_Easy;
+
+public class Program9 {
+
+    static int transitionPoint( int arr[], int N){
+
+        for( int i = 0; i < N; i++){
+
+            if( arr[i] == 1){
+
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = { 0, 0, 0, 1, 1, 1};
+
+        int N = arr.length;
+
+        System.out.println(transitionPoint( arr, N));
+    }
+    
+}
