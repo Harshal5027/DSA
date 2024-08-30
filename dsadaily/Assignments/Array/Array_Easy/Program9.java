f733a3b2d674020dd6b98e62124dd26ae11fb48c
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-08-30 20:09:55 +0530

    Transition Point

--- dsadaily/Assignments/Array/Array_Easy/Program9.java
+++ dsadaily/Assignments/Array/Array_Easy/Program9.java
@@ -19,7 +19,7 @@ public class Program9 {
 
     public static void main(String[] args) {
         
-        int arr[] = { 0, 0, 0, 1, 1, 1};
+        int arr[] = { 0, 0, 0, 0, 0, 0};
 
         int N = arr.length;
 
