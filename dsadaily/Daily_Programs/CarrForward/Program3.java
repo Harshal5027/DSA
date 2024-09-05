51d8c39431db90b6ca165d11eb0ec42b3da4573d
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-05 22:37:06 +0530

    Right max Array

---
+++ dsadaily/Daily_Programs/CarrForward/Program3.java
@@ -0,0 +1,26 @@
+package Daily_Programs.CarrForward;
+
+
+public class Program3 {
+    public static void main(String[] args) {
+        int arr[]=new int[]{-3,6,2,4,5,2,8,-9,3,1};
+        int N=10;
+        
+        int[] rightmax=new int[N];
+       // int max=Integer.MIN_VALUE;
+        rightmax[N-1]=arr[N-1];
+
+        for(int i=N-2; i>=0; i--){
+
+            if(arr[i]> rightmax[i+1]){
+                rightmax[i]=arr[i];
+            }else{
+                rightmax[i]=rightmax[i+1];
+            }
+        }
+        for( int i = 0; i < N; i++){
+
+            System.out.print(rightmax[i]+" ");
+        }
+    }
+}
