d4515e407a120761694b7ad77186cb5919fda8c8
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-07 12:38:19 +0530

    2Darray

--- dsadaily/Daily_Programs/TwoDMatrices/Program7.java
+++ dsadaily/Daily_Programs/TwoDMatrices/Program7.java
@@ -13,8 +13,8 @@ public class Program7 {
 
         arr[0] = new int[]{ 1, 2, 3, 4, 5};
         arr[1] = new int[]{ 6, 7, 8, 9, 10};
-        arr[2] = new int[]{ 11, 12, 13, 14, 19};
-        arr[3] = new int[]{ 15, 16, 17, 18, 20};
+        arr[2] = new int[]{ 11, 12, 13, 14, 15};
+        arr[3] = new int[]{ 16, 17, 18, 19, 20};
         
         int newarr[][] = new int[5][4];
 
@@ -30,6 +30,7 @@ public class Program7 {
             for(int j=0; j<newarr[0].length; j++) {
                 System.out.print(newarr[i][j]+" ");
             }
+            System.out.println();
         }
     }
 }
