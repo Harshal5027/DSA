7da7c3b83169b5ddbc116f1f0a82791d840e8c06
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-18 18:07:09 +0530

    Rotation

--- dsadaily/Assignments/Array/Array_Easy/Program16.java
+++ dsadaily/Assignments/Array/Array_Easy/Program16.java
@@ -1,5 +1,41 @@
+
+//      Rotation 
+
 package Assignments.Array.Array_Easy;
 
 public class Program16 {
+
+    static int rotation( int arr[], int N ){
+
+        int max = Integer.MIN_VALUE;
+
+        int count = 0;
+
+        for( int i = 0; i < N; i++ ){
+
+            if( arr[i] > max )
+                max = arr[i];
+        }
+
+        for( int i = 0 ; i < N; i++ ){
+
+            if( arr[i] == max )
+                count = i;
+        }
+
+        if( count == N-1 )
+            return 0;
+        else
+            return count+1;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = { 5, 1, 2, 3, 4 };
+
+        int N = arr.length;
+
+        System.out.println(rotation( arr, N ));
+    }
     
 }
