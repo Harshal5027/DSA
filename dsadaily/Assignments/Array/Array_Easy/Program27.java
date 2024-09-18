255a94166ae6ddb9c9f6dd3ef4a13404d8d0cb7e
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-18 13:46:50 +0530

    Remove the Duplicate Elements from the sorted Array

--- dsadaily/Assignments/Array/Array_Easy/Program27.java
+++ dsadaily/Assignments/Array/Array_Easy/Program27.java
@@ -27,7 +27,7 @@ public class Program27 {
 
     public static void main(String[] args) {
         
-        int arr[] = { 2, 2, 2, 2, 2};
+        int arr[] = { 1, 2, 2, 4};
 
         int N = arr.length;
 
