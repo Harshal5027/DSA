8cdcf18353765b381d10c646eb181af180088eef
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-18 17:08:51 +0530

    Peak Element

--- dsadaily/Assignments/Array/Array_Easy/Program5.java
+++ dsadaily/Assignments/Array/Array_Easy/Program5.java
@@ -4,5 +4,37 @@
 package Assignments.Array.Array_Easy;
 
 public class Program5 {
+
+    static int peakElement( int arr[], int N ){
+
+        for( int i = 0; i < N ; i++ ){
+
+            if( i == 0 ){
+
+                if( arr[i+1] < arr[i] )
+                    return 1;
+
+                continue;
+            }
+            if( i == N-1 ){
+
+                if( arr[i-1] < arr[i])
+                    return 1;
+            }
+            if( arr[i-1] < arr[i] && arr[i+1] < arr[i] )
+                return 1;
+                
+        }
+
+        return 0;
+    }
     
+    public static void main(String[] args) {
+        
+        int arr[] = { 1, 2, 3};
+
+        int N = arr.length;
+
+        System.out.println(peakElement( arr, N));
+    }
 }
