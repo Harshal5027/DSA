88917c9a1af835c26c7f8bad287a729c8fedc9e7
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-20 20:45:52 +0530

    Insertion Sort

---
+++ dsadaily/Daily_Programs/Sorting/InsertionSort.java
@@ -0,0 +1,38 @@
+package Daily_Programs.Sorting;
+
+public class InsertionSort {
+
+    static void insertionSort( int arr[], int N ){
+
+        for( int i = 0; i < N; i++){
+
+            int element = arr[i];
+
+            int j = i - 1;
+
+            while( j >= 0 && arr[j] > element){
+
+                arr[j+1] = arr[j];
+
+                j--;
+            }
+
+            arr[j+1] = element; 
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = new int[] { 8, 3, 1, 7, 5, 4, 2};
+
+        int N = arr.length;
+
+        insertionSort( arr, N);
+
+        for( int i = 0; i < N; i++){
+
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
