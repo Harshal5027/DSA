ddd14c5915e1dccef484438bee92db32f946e11a
Harshal5027 <harshalkhaire.edu@gmail.com>
2024-09-28 20:51:57 +0530

    CarrayForward

--- dsadaily/Daily_Programs/Array/CarrForward/Program2.java
+++ dsadaily/Daily_Programs/Array/CarrForward/Program2.java
@@ -1,24 +1,38 @@
-package Daily_Programs.CarrForward;
+/*
+ * Given integer array of size N
+ * build an array leftmax of size N
+ * leftmax of i contains the maximum for the index 0 to the index i 
+ * 
+ * Arr : [ -3, 6, 2, 4, 5, 2, 8, -2, 3, 1]
+ * N = 10
+ * leftMax : [ -3, 6, 6, 6, 6, 6, 8, 8, 8]
+ * 
+ */
+
+
+package Daily_Programs.Array.CarrForward;
 
 public class Program2 {
     public static void main(String[] args) {
-        int arr[]=new int[]{-3,6,2,4,5,2,8,-9,3,1};
-        int N=10;
+        int arr[] = new int[]{-3,6,2,4,5,2,8,-9,3,1};
+        int N = arr.length;
         
-        int[] leftmax=new int[N];
+        int[] leftmax = new int[N];
        // int max=Integer.MIN_VALUE;
-        leftmax[0]=arr[0];
+        leftmax[0] = arr[0];
 
-        for(int i=1; i<N; i++){
+        for(int i = 1; i < N; i++){
 
-            if(arr[i]>leftmax[i-1]){
-                leftmax[i]=arr[i];
+            if(arr[i] > leftmax[i-1]){
+                leftmax[i] = arr[i];
             }else{
-                leftmax[i]=leftmax[i-1];
+                leftmax[i] = leftmax[i-1];
             }
         }
-        
 
+        for( int i = 0; i < N; i++){
 
+            System.out.print(leftmax[i]+" ");
+        }
     }
 }
